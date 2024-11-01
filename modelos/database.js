2c5fac0eb0e6944c6091335f9c2af89a7cc287fd
pedrosjc10 <62314688+pedrosjc10@users.noreply.github.com>
2024-10-31 22:23:37 -0300

    criada a rota user e modelo user

--- modelos/database.js
+++ modelos/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'teste123',
+    password: '',
     database: 'projeto_4bim'
 });
 
---
+++ router/userRouter.js
@@ -0,0 +1,88 @@
+const express = require('express');
+
+const UserControl = require('../control/UserControl');
+
+const UserMiddleware = require('../middleware/UserMiddleware');
+const JWTMiddleware = require('../middleware/JWTMiddleware');
+
+module.exports = class UserRouter {
+
+    constructor() {
+        this._router = express.Router();
+        this._jwtMiddleware = new JWTMiddleware();
+        this._userControl = new UserControl();
+        this._userMiddleware = new UserMiddleware();
+    }
+
+    createRoutes() {
+        const multer = require('multer');
+        const upload = multer({ dest: 'uploads/' });
+
+        this.router.post('/csv',
+            this.jwtMiddleware.validate,
+            upload.single('userFile'),
+            this.userControl.createByCSV
+        );
+
+        this.router.get('/',
+            this.jwtMiddleware.validate,
+            this.userControl.readAll
+        );
+
+        this.router.get('/:idUser',
+            this.jwtMiddleware.validate,
+            this.userControl.readById
+        );
+
+        this.router.post('/',
+            this.jwtMiddleware.validate,
+            this.userMiddleware.validateUserData,
+            this.userMiddleware.isUniqueUser,
+            this.userControl.create
+        );
+
+        this.router.delete('/:idUser',
+            this.jwtMiddleware.validate,
+            this.userControl.delete
+        );
+
+        this.router.put('/:idUser',
+            this.jwtMiddleware.validate,
+            this.userControl.update
+        );
+
+        return this.router;
+    }
+
+    get router() {
+        return this._router;
+    }
+
+    set router(newRouter) {
+        this._router = newRouter;
+    }
+
+    get jwtMiddleware() {
+        return this._jwtMiddleware;
+    }
+
+    set jwtMiddleware(newJwtMiddleware) {
+        this._jwtMiddleware = newJwtMiddleware;
+    }
+
+    get userControl() {
+        return this._userControl;
+    }
+
+    set userControl(newUserControl) {
+        this._userControl = newUserControl;
+    }
+
+    get userMiddleware() {
+        return this._userMiddleware;
+    }
+
+    set userMiddleware(newUserMiddleware) {
+        this._userMiddleware = newUserMiddleware;
+    }
+}
